9307f69a2eb2b05dcff8a4c42449fca4d05d8b5b
Gutus Nirwanto <gutusnirwanto@gmail.com>
2021-10-21 04:39:22 +0700

    master

--- Day04/01_random_module.py
+++ Day04/01_random_module.py
@@ -1,7 +1,21 @@
 #### Random Module ####
 import random
 
-# Angka integer acak antara 1 hingga 3 tanpa 3 (acak antara 1-2)
-random_integer = random.randint(1, 3)
+# Integer number acak antara 1 hingga 3
+random_Integer = random.randint(1, 3)
 
-print(random_integer)
+print(random_Integer)
+
+# Float Number acak menggunakan random.random()
+randomFloat = random.random() * 5
+print(f"Ini adalah float number acak >>> {randomFloat}")
+
+
+# Love score menggunakan integer random
+loveScore = random.randint(1, 100)
+if loveScore < 50:
+    print(
+        f"Your love score {loveScore} is too low now, please try again later!")
+else:
+    print(
+        f"Congratulation your Love Score is {loveScore}, yo can go to the next level now!")
